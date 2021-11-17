625f5d055e92e0d392da0a69551d46b4efc01ad0
Abdi <ah1704@live.mdx.ac.uk>
2021-11-17 14:07:23 +0000

    stored all my lessons in array in a seperate js file

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    { 
+        id: 1001, 
+        subject: "English",
+        location: "England",
+        price: 1.99,
+        spaces: 5,
+        image: 'icons/english icon.jpg',
+        rating: 1
+    }, 
+    { 
+        id: 1002,
+        subject: "Math",
+        location: "Scotland",
+        price: 4.99,
+        spaces: 5,
+        image: "icons/math icon.png",
+        rating: 2   
+    },
+    {
+        id: 1003,
+        subject: "Science",
+        location: "Wales",
+        price: 9.99,
+        spaces: 5,
+        image: "icons/science icon.png",
+        rating: 3
+    },
+    {
+        id: 1004,
+        subject: "History",
+        location: "France",
+        price: 19.99,
+        spaces: 5,
+        image: "icons/history icon.jpg",
+        rating: 4
+    },
+    {
+        id: 1005,
+        subject: "Geography",
+        location: "Ireland",
+        price: 29.99,
+        spaces: 5,
+        image: "icons/geography icon.png",
+        rating: 5
+    },
+    {
+        id: 1006,
+        subject: "Music",
+        location: "France",
+        price: 39.99,
+        spaces: 5,
+        image: "icons/music icon.jpg",
+        rating: 1
+    },
+    {
+        id: 1007,
+        subject: "Dance",
+        location: "Spain",
+        price: 49.99,
+        spaces: 5,
+        image: "icons/dance icon.png",
+        rating: 2
+    },
+    {
+        id: 1008,
+        subject: "ICT",
+        location: "China",
+        price: 59.99,
+        spaces: 5,
+        image: "icons/ICT icon.png",
+        rating: 3
+    },
+    {
+        id: 1009,
+        subject: "PE",
+        location: "Russia",
+        price: 69.99,
+        spaces: 5,
+        image: "icons/PE icon.png",
+        rating: 4
+    },
+    {
+        id: 1010,
+        subject: "German",
+        location: "Germany",
+        price: 79.99,
+        spaces: 5,
+        image: "icons/german icon.png",
+        rating: 5
+    }
+]
